98737337502469819854abd52efc74201b29bd29
ettienekorayyi <corralstephenmelben@gmail.com>
2020-05-29 15:42:24 +1000

    Implemented the feature that changes the background depending on the season. Current Limitation is that only determines the Southern hemisphere season.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,53 @@ import React from 'react';
 
 import Weather from './components/Weather/Weather';
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
-import './App.css'
+
+import SummerBackground from './assets/images/summer-image.jpg';
+import AutumnBackground from './assets/images/autumn-image-mobile.jpg';
+import WinterBackground from './assets/images/winter-image-mobile.jpg';
+import SpringBackground from './assets/images/spring-image-mobile.jpg';
+import './App.css';
 
 class App extends React.Component {
   state = {
     lat: '',
     lon: '',
+    date: '',
+    month: '',
     errorMessage: ''
   };
 
   async componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      ({ coords }) => this.setState({ lat: coords.latitude, lon: coords.longitude }),
+      ({ coords }) => this.setState({ 
+        lat: coords.latitude, 
+        lon: coords.longitude, 
+        date: new Date().toUTCString(),
+        month: new Date().getMonth()
+      }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
 
+  imageRenderer = () => {
+    const { month } = this.state;
+    
+    if(month >= 2 && month <= 4) {
+      return AutumnBackground;
+    } else if(month >= 5 && month <= 7) {
+      return WinterBackground;
+    } else if(month >= 8 && month <= 10) {
+      return SpringBackground;
+    } else if((month >= 0 && month <= 1) 
+    || month === 11) {
+      return SummerBackground;
+    }
+  };
+
   renderContent = () => {
-    if (this.state.errorMessage && !this.state.lat) {
+    const { date, errorMessage, lat, lon } = this.state;
+
+    if (errorMessage && !lat) {
       return (
         <div className="ui active dimmer">
           <h1 style={{ color: 'white' }}>Error: {this.state.errorMessage}</h1>
@@ -27,12 +56,13 @@ class App extends React.Component {
       )
     }
 
-    if (!this.state.errorMessage && (this.state.lat && this.state.lon)) {
+    if (!errorMessage && (lat && lon)) {
       return (
-        <div className="App">
+        <div className="App" style={{ backgroundImage: `url(${this.imageRenderer()})` }}>
           <Weather
-            lat={this.state.lat}
-            lon={this.state.lon}
+            lat={lat}
+            lon={lon}
+            date={date.substring(0, date.length - 13).toString()}
           />
         </div>
       );
@@ -44,7 +74,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {this.renderContent()}
+        { this.renderContent() }
       </div>
     );
   }
