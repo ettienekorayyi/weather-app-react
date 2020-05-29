ec231c9768947347ccd5dcde8c77899ec6995e1f
ettienekorayyi <corralstephenmelben@gmail.com>
2020-05-29 19:42:21 +1000

    fixed media query issue

--- src/api/openweathermap.js
+++ src/api/openweathermap.js
@@ -4,8 +4,14 @@ export const apiKey = {
     accessKey: 'acc9d7a1bb44c63eb0003bc8ae3c3e19'
 };
 
+const protocol = () => {
+    return window.location.protocol === 'http'
+        ? 'http'
+        : 'https'
+};
+
 export default axios.create({
-    baseURL: 'https://api.openweathermap.org/data/2.5/',
+    baseURL: `${protocol()}://api.openweathermap.org/data/2.5/`,
     timeout: 2000,
     headers: {
         'Content-Type': 'application/json'
